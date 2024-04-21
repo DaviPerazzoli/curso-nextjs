240aff54c3473916e1bfde7a87ed6a8e0fedec11
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-04-21 17:33:29 -0300

    aprendi a criar um 404 custom

---
+++ next-routing/pages/404.tsx
@@ -0,0 +1,5 @@
+const PageNotFound: React.FC = () => {
+    return <h1>404 Page with all the custom styling necessary</h1>
+}
+
+export default PageNotFound;
--- next-routing/pages/index.tsx
+++ next-routing/pages/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home: React.FC = () => {
+
+  const router = useRouter();
+
+  const handleClick = (): void => {
+    console.log('Placing your order');
+    // router.push('/product');
+    router.replace('/product');
+  }
   return( 
     <div>
       <h1>Home Page</h1>
@@ -10,6 +19,10 @@ const Home: React.FC = () => {
       </Link>
       <br/>
       <Link href='/product'>Products</Link>
+      <br/>
+      <button onClick={handleClick}>
+        Place Order
+      </button>
     </div>
     )
 }
