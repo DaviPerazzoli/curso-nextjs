3056686205aea7fd2527d8e47d609609c42ceb42
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-04-21 17:21:38 -0300

    aprendi Link

--- next-routing/pages/product/[productId]/index.tsx
+++ next-routing/pages/product/[productId]/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 const ProductDetail: React.FC = () => {
     const router = useRouter();
     const productId: number = Number(router.query.productId)
-    return <h1>Details about Product {productId + 1}</h1>
+    return <h1>Details about Product {productId}</h1>
 }
 
 export default ProductDetail;
--- next-routing/pages/product/index.tsx
+++ next-routing/pages/product/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Link from "next/link";
 
-const ProductList: React.FC = () => {
+const ProductList: React.FC<{productId: string | number}> = ( {productId=100} ) => {
     return(
         <>
         <Link href="/">Home</Link>
         <ul>
-            <li key={1}>Product 1</li>
-            <li key={2}>Product 2</li>
-            <li key={3}>Product 3</li>
+            <li key={1}><Link href="/product/1">Product 1</Link></li>
+            <li key={2}><Link href="/product/2">Product 2</Link></li>
+            <li key={3}><Link replace href="/product/3">Product 3</Link></li>
+            <li key={4}><Link href={"/product/" + productId}>Product {productId}</Link></li>
         </ul>
         </>
     )
