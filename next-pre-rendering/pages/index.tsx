326417d68bbd04d5d746490cc11dbac74369824a
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-04-23 17:48:23 -0300

    aprendi build

--- next-pre-rendering/pages/index.tsx
+++ next-pre-rendering/pages/index.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Home: React.FC = () => {
   return <>
-  <h1>Next JS pre-rendering</h1>
-  <Link href="/users">Users</Link>
+    <h1>Next JS pre-rendering</h1>
+    <Link href="/users">Users</Link>
   </>
 }
 
