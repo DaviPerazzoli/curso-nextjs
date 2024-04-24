6b69c936564187c0d734924f0c358e1493babe34
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-04-24 18:18:40 -0300

    aprendi getStaticPaths: fallback true e false

--- next-pre-rendering/pages/posts/[postId].tsx
+++ next-pre-rendering/pages/posts/[postId].tsx
@@ -2,6 +2,12 @@ import { useRouter } from "next/router";
 import { PostJSON } from "./index";
 
 const Post: React.FC<{post: PostJSON}> = ({ post }) => {
+    const router = useRouter();
+
+    if(router.isFallback){
+        return <h1>Loading...</h1>
+    }
+
     return (<>
         <h1>{post.id} {post.title}</h1>
         <p>{post.body}</p>
@@ -9,6 +15,20 @@ const Post: React.FC<{post: PostJSON}> = ({ post }) => {
 }
 
 export async function getStaticPaths() {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data: PostJSON[] = await response.json();
+
+   
+
+    const paths = data.map(post => {
+        return {
+            params: {
+                postId: post.id.toString()
+            }
+        }
+    })
+    
     return {
         paths: [
             {
@@ -21,7 +41,8 @@ export async function getStaticPaths() {
                 params: { postId: '3' }
             },
         ],
-        fallback: false,
+        // paths,
+        fallback: true,
     }
 }
 
@@ -32,6 +53,14 @@ export async function getStaticProps( context: any ) {
         `https://jsonplaceholder.typicode.com/posts/${params.postId}`
     );
     const data: PostJSON = await response.json();
+
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
+
+    console.log('Generating page for /posts/'+params.postId);
 
     return {
         props: {
