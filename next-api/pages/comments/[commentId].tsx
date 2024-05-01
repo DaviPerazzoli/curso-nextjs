e490710e862d0bbde8e6415bff2f57bcf769ca1c
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-04-30 22:14:02 -0300

    terminei de aprender sobre api routing

--- next-api/pages/comments/[commentId].tsx
+++ next-api/pages/comments/[commentId].tsx
@@ -6,8 +6,11 @@ import type {
 } from 'next';
 
 function CommentDetails ({
-     comment 
+     comment
     }: InferGetStaticPropsType<typeof getStaticProps>) {
+        if(!comment){
+            return 
+        }
         return <div key={comment.id}>
             {comment.text}
         </div>
@@ -43,15 +46,21 @@ export const getStaticProps = ( async ( context ) => {
     }
 
     const commentId = Number(params.commentId);
-
-    const response = await fetch('/api/comments');
+    
+    const response = await fetch('http://localhost:3000/api/comments');
     const data: Comment[] = await response.json();
-    console.log(data);
-    const comment = data[commentId];
+    
+    if (commentId > data.length || commentId === 0){
+        return {
+            notFound: true
+        }
+    }
+
+    const comment = data[commentId-1];
 
     return {
         props: {
             comment
         }
     }
-}) satisfies GetStaticProps<{ comment: Comment }>;
+}) satisfies GetStaticProps<{ comment: Comment}>;
