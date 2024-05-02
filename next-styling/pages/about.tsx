0ba60ab60c6efa7b611bf5e8786b62e52eadd659
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-01 22:03:59 -0300

    aprendi styling em nextjs

---
+++ next-styling/pages/about.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/About.module.css'
+import stylesscss from '../styles/About.module.scss'
+
+const About: React.FC = () => {
+    return <button className={stylesscss.highlightscss}>Primary</button>
+}
+
+export default About;
---
+++ next-styling/pages/contact.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Contact.module.css'
+
+const Contact: React.FC = () => {
+    return <h2 className={styles.highlight}>Contact Page</h2>
+}
+
+export default Contact;
