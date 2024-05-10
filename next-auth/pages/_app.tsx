78a353243790c667ce53be746c8d219036f603ea
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-10 11:39:15 -0300

    aprendendo server-side authentication

--- next-auth/pages/_app.tsx
+++ next-auth/pages/_app.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
--- next-auth/pages/blog.tsx
+++ next-auth/pages/blog.tsx
@@ -1,5 +1,20 @@
-function Blog() {
-    return <h1>Blog Page</h1>
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getSession , useSession} from "next-auth/react";
+
+function Blog({data}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    const {data: session, status} = useSession();
+    return <h1>Blog page - {data}</h1>
 }
 
 export default Blog;
+
+export const getServerSideProps = (async (context) => {
+    const session = await getSession(context);
+
+    return {
+        props: {
+            session,
+            data: session ? 'List of 100 blog posts': 'List of free blog posts'
+        }
+    }
+}) satisfies GetServerSideProps<{data: string}>
