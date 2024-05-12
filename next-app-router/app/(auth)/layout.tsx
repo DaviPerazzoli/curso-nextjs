64d1be50d1935e687543517ca1b6ed400a4f465e
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-12 11:28:02 -0300

    aprendendo as novidades do app router

---
+++ next-app-router/app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+    {name: 'Register', href: '/register'},
+    {name: 'Login', href: '/login'},
+    {name: 'Forgot Password', href: '/forgot-password'},
+]
+
+export default function AuthLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    const pathName = usePathname();
+
+    
+    return (<div>
+        {navLinks.map( link => {
+            const isActive = pathName.startsWith(link.href)
+            return (<Link 
+                href={link.href} 
+                key={link.name} 
+                className={isActive? 'bold': ''}>
+                    {link.name}
+                    </Link>)
+        })}
+        {children}
+    </div>)
+}
