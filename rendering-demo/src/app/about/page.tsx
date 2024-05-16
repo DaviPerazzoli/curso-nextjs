cce3f4cea22fb6ec019d3181c3583bcdf95f4ff0
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-16 20:01:31 -0300

    aprendendi static e dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
+import { cookies } from "next/headers";
+
 export default function AboutPage() {
+    const cookieStore = cookies();
+    const theme = cookieStore.get('theme');
+    console.log(theme);
     console.log('About component');
-    return <h1>About Page</h1>;
+    return <h1>About Pag {new Date().toLocaleTimeString()}</h1>;
 }
