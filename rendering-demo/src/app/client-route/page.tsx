5bad0069cab56f7e8205d53c3aab34de4361bd10
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-17 11:47:47 -0300

    aprendi a trabalhar com context em server-components

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,38 +1,12 @@
 "use client";
 
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoute() {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
-      return (
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      );
+  const theme = useTheme();
+  return (
+    <h1 style={{
+      color: theme.colors.secondary
+    }}>Client route</h1>
+  )
 }
